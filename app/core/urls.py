a507913c00212356ccbc557bc62de0ede939132b
Chris Harrel <harrelchris@gmail.com>
2025-07-18 02:22:56 -0500

    Update urls

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,23 +1,36 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views import defaults
+
+admin.site.index_title = admin.site.name = admin.site.site_header = admin.site.site_title = "BVA Search Admin"
 
 urlpatterns = [
+    path("", include("decisions.urls"), name="decisions"),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+    urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+    urlpatterns.extend(
+        [
+            path(
+                "400/",
+                defaults.bad_request,
+                kwargs={"exception": Exception("Bad Request")},
+            ),
+            path(
+                "403/",
+                defaults.permission_denied,
+                kwargs={"exception": Exception("Permission Denied")},
+            ),
+            path(
+                "404/",
+                defaults.page_not_found,
+                kwargs={"exception": Exception("Not Found")},
+            ),
+            path("500/", defaults.server_error),
+        ],
+    )
