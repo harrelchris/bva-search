be563bbda32bcf0084eec2a5ec60ed93598f8097
Chris Harrel <harrelchris@gmail.com>
2025-07-19 13:03:29 -0500

    Update pk type

--- app/decisions/urls.py
+++ app/decisions/urls.py
@@ -7,5 +7,5 @@ app_name = "decisions"
 urlpatterns = [
     path("", views.SearchView.as_view(), name="search"),
     path("results/", views.ResultsView.as_view(), name="results"),
-    path("decision/<int:pk>/", views.DecisionView.as_view(), name="decision"),
+    path("decision/<str:pk>/", views.DecisionView.as_view(), name="decision"),
 ]
